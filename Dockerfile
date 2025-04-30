87bf1f05e9b65957a566a3534db4683a9f613b6f
Alexitic0 <alex.vega@duocuc.cl>
2025-04-30 17:53:58 -0400

    2do comentario jeje

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,6 @@
 FROM python
 RUN pip install flask
 COPY  sample_app.py /home/myapp/
-EXPOSE 8000
-CMD python3 /home/myapp/sample_app.py
-FROM python
-RUN pip install flask
-COPY  sample_app.py /home/myapp/
+COPY index.html /home/myapp/templates/
 EXPOSE 8000
 CMD python3 /home/myapp/sample_app.py
--- scriptbash.sh
+++ scriptbash.sh
@@ -1,8 +1,10 @@
-echo "FROM python" >> Dockerfile
+echo "FROM python" > Dockerfile
 echo "RUN pip install flask" >> Dockerfile
 echo "COPY  sample_app.py /home/myapp/" >> Dockerfile
+echo "COPY index.html /home/myapp/templates/" >> Dockerfile
 echo "EXPOSE 8000" >> Dockerfile
 echo "CMD python3 /home/myapp/sample_app.py" >> Dockerfile
+
 
 docker build -t web .
 docker run -d -p 8000:8000 --name web web
